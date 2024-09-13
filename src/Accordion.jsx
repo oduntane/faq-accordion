4df2555d99432c937b141fdacbee205447bf6f13
Emmanuel Oduntan <oduntane@icloud.com>
2024-09-13 04:01:31 +0100

    Extract accordion component into a separate file

---
+++ src/Accordion.jsx
@@ -0,0 +1,92 @@
+import {useState} from 'react'
+import minusIcon from './assets/icon-minus.svg'
+import plustIcon from './assets/icon-plus.svg'
+
+const faqList = [
+    {
+        title: "What is Frontend Mentor, and how will it help me?",
+        content: `
+            Frontend Mentor offers realistic coding challenges to help 
+            developers improve their frontend coding skills with projects in
+            HTML, CSS, and JavaScript. It's suitable for all levels and ideal
+            for portfolio building.
+        `
+    },
+    {
+        title: "Is Frontend Mentor free?",
+        content: `
+            Yes, Frontend Mentor offers both free and premium coding challenges,
+            with the free option providing access to a range of projects 
+            suitable for all skill levels.
+        `
+    },
+    {
+        title: "Can I use Frontend Mentor projects in my portfolio?",
+        content: `
+            Yes, you can use projects completed on Frontend Mentor in your
+            portfolio. It's an excellent way to showcase your skills to 
+            potential employers!
+        `
+    },
+    {
+        title: "How can i get help if I'm stuck on a Frontend Mentor challenge?",
+        content: `
+            The best place to get help is inside Frontend Mentor's Discord 
+            community. There's a help channel where you can ask questions and 
+            seek support from other community members.
+        `
+    }
+]
+
+function AccordionItem({index, title, children, selected, onSelectedChange}) {
+
+
+    return (
+        <div className="accordion">
+          <div>
+            <h2>{title}</h2>
+            <span onClick={() => {
+                selected ? onSelectedChange(null) : onSelectedChange(index)
+            }}>
+                {
+                    selected ?
+                    <img src={minusIcon} alt="Active"/>:
+                    <img src={plustIcon} alt="Not Active" />
+                }
+              
+            </span>
+          </div>
+          {
+            selected && <p>
+            {children}
+          </p>
+          }
+        </div>
+    )
+}
+
+export default function Accordion() {
+    const [selected, setSelected] = useState(null)
+
+
+    return (
+        <>
+        {
+            faqList.map((item, index) => {
+                return (
+                    <AccordionItem key={index} title={item.title} 
+                                selected={selected == index}
+                                index={index}
+                                onSelectedChange={setSelected}>
+                                    {
+                                        item.content
+                                    }
+                    </AccordionItem>
+                )
+            })
+        }
+        
+        </>
+    )
+
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import minusIcon from './assets/icon-minus.svg'
-import plusIcon from './assets/icon-plus.svg'
-
+import Accordion from './Accordion'
 
 export default function App() {
 
@@ -8,59 +6,7 @@ export default function App() {
     <main>
       <div className="container">
         <h1>FAQS</h1>
-        <div className="accordion">
-          <div>
-            <h2>What is Frontend Mentor, and how will it help me?</h2>
-            <span>
-              <img src={minusIcon} alt="Active"/>
-            </span>
-          </div>
-          <p>
-            Frontend Mentor offers realistic coding challenges to help 
-            developers improve their frontend coding skills with 
-            projects in HTML, CSS, and JavaScript.
-            It's suitable for all levels and ideal for portfolio building.
-          </p>
-        </div>
-        <div className="accordion">
-          <div>
-            <h2>Is Frontend Mentor free?</h2>
-            <span>
-              <img src={plusIcon} alt="Active"/>
-            </span>
-          </div>
-          <p>
-            Yes, Frontend Mentor offers both free and premium challenges, with 
-            the free option providing access to a range of projects suitable 
-            for all skill levels.
-          </p>
-        </div>
-        <div className="accordion">
-          <div>
-            <h2>Can I use Frontend Mentor projects in my portfolio?</h2>
-            <span>
-              <img src={plusIcon} alt="Active"/>
-            </span>
-          </div>
-          <p>
-            Yes, you can use projects completed on Frontend Mentor in your
-            portfolio. It's an excellent way to showcase your skills to 
-            potential employers!
-          </p>
-        </div>
-        <div className="accordion">
-          <div>
-            <h2>How can I get help if I'm stuck on a Frontend Mentor challenge?</h2>
-            <span>
-              <img src={plusIcon} alt="Active"/>
-            </span>
-          </div>
-          <p>
-            The best place to get help is inside Frontend Mentor's Discord 
-            community. There's a help channel where you can ask questions and 
-            seek support from other community members.
-          </p>
-        </div>
+        <Accordion />
       </div>
     </main>
   )
